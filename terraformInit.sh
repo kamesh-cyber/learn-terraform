0e9abcd476e578e6f5f46c7fb5e22aad660ca13a
kamesh-cyber <79978406+kamesh-cyber@users.noreply.github.com>
2024-03-28 19:12:49 +0530

    Update terraformInit.sh

--- terraformInit.sh
+++ terraformInit.sh
@@ -9,7 +9,9 @@ status=$(terraform workspace list | grep -E "${tfworkspaceName}\$")
 # Check if the return status is 0
 if [ $? -eq 0 ]; then
     # If the workspace exists, select it
+    echo "select existing workspace"
     terraform workspace select "${tfworkspaceName}"
 else
+  echo "create new workspace"
   terraform workspace new "${tfworkspaceName}"
 fi

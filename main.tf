668bee046b8c233fa489cebf4d73270005da03fb
kamesh-cyber <79978406+kamesh-cyber@users.noreply.github.com>
2024-03-28 17:56:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "local_file" "pet"{
     filename = "test.txt"
-    content = "This is a file generated using terraform"
+    content = "This is a file generated using terraform testing"
     file_permission = 0700
 
 }

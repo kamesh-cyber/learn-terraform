dc4da8cb416d1bc53b873c6db2facd1228b69550
kamesh-cyber <79978406+kamesh-cyber@users.noreply.github.com>
2024-03-28 19:09:50 +0530

    Create terraformInit.sh

---
+++ terraformInit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define the workspace name
+tfworkspaceName="prod"
+
+# Execute the command and capture its return status
+status=$(terraform workspace list | grep -E "${tfworkspaceName}\$")
+
+# Check if the return status is 0
+if [ $? -eq 0 ]; then
+    # If the workspace exists, select it
+    terraform workspace select "${tfworkspaceName}"
+fi
